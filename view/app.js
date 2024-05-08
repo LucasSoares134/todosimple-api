1d2b610dfbe6c16cd9822753dd0f192290e0a681
LucasSoares134 <soareslucas134@gmail.com>
2024-05-08 00:25:44 -0300

    inicio de front

---
+++ view/app.js
@@ -0,0 +1,15 @@
+const url = "http://localhost:8080/task/user/1";
+
+function hideLoader(){
+    document.getElementsById("loading").style.display = "none";
+}
+function show(task) {
+    let tab = `<thead>
+                <th scope="col">#<th>
+                <th scope="Description">#<th>
+                <th scope="col">Username<th>
+                <th scope="col">User id<th>
+                </thead>;
+    `
+//escrevendo html dentro do java sript
+}
